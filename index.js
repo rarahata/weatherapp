59d4dd08e5d8d1d8d16e3aea688c3029f6abfa34
rarahata <rei.arahata@gmail.com>
2021-09-16 23:04:31 +0100

    add geolocation and show temperature

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ function showTemperature(response){
   currentTemp.innerHTML = `${tempData}°C`;
 }
 
-
 function search (event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-Input");
@@ -17,3 +16,18 @@ function search (event) {
 
 let currentCity = document.querySelector(".search-form");
 currentCity.addEventListener ("submit",search);
+
+
+function showPosition(position){
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7796ed76d4738ed90e39d5875eb78f75&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentPosition(){
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let currentPosition = document.querySelector("#current-position-button");
+currentPosition.addEventListener("click", getCurrentPosition);
