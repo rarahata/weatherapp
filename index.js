4fc78191011928879adcdba993187ced60c40d56
rarahata <rei.arahata@gmail.com>
2021-09-16 22:29:31 +0100

    add function search

---
+++ index.js
@@ -0,0 +1,14 @@
+function search (event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-Input");
+  let cityName = `${cityInput.value}`;
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = cityName;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7796ed76d4738ed90e39d5875eb78f75&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+}
+
+
+let currentCity = document.querySelector(".search-form");
+currentCity.addEventListener ("submit",search);
